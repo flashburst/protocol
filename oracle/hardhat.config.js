f644ae1ed2a2e402b2a7aeeb38587dacbf4b0849
flashburst <82953782+flashburst@users.noreply.github.com>
2022-05-26 07:14:03 +0530

    Added scripts for deployment

--- oracle/hardhat.config.js
+++ oracle/hardhat.config.js
@@ -9,10 +9,39 @@ require('dotenv').config()
  * @type import('hardhat/config').HardhatUserConfig
  */
 const config = {
-  defaultNetwork: 'local',
+  defaultNetwork: 'hardhat',
   networks: {
+    hardhat: {
+      // forking: {
+      //   url: process.env.ALCHEMY_API_URL,
+      //   blockNumber: 11819774
+      // }
+    },
+    ropsten: {
+      url: `${process.env.ROPSTEN_RPC_URL}`,
+      chainId: 3,
+      accounts: [process.env.PRIVATE_KEY],
+      gasPrice: 15000000000,
+      gas: 'auto'
+    },
+    bscTestnet: {
+      url: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
+      chainId: 97,
+      accounts: [process.env.PRIVATE_KEY],
+      // gasPrice: 30000000000,
+      gas: 'auto'
+    },
+    mumbai: {
+      url: 'https://rpc-mumbai.maticvigil.com/',
+      chainId: 80001,
+      accounts: [process.env.PRIVATE_KEY],
+      // gasPrice: 30000000000,
+      gas: 'auto'
+    },
     local: {
-      url: 'http://localhost:24012/rpc'
+      url: 'http://localhost:8545/',
+      chainId: 1337,
+      accounts: [process.env.PRIVATE_KEY]
     }
   },
   solidity: {
